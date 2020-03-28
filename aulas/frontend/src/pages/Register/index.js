e8ee19ac3bafab5aba4ed84a457c6b01fe3ec52b
Jabes Bueno <jabesbueno@hotmail.com>
2020-03-28 00:59:29 -0300

    Construindo a interface web

---
+++ aulas/frontend/src/pages/Register/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+
+export default function Register(){
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
+                
+                    <Link className="back-link" to="/">
+                        <FiLogIn size={16} color="#f02041"/>
+                        Não tenho cadastro
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder="WhatsApp" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{width: 80}} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
